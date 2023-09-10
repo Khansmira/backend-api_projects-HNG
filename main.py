44472863697fe5a3e386d77a2dabfe9312d3839a
Khansmira <miriamakanwa@gmail.com>
2023-09-10 08:57:12 -0700

    fix:endpoint main function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import datetime
 import pytz
-import os
+
 app = Flask(__name__)
 
 # Define the route for the endpoint
@@ -10,6 +10,7 @@ def get_info():
     # Get query parameters
     slack_name = request.args.get('slack_name')
     track = request.args.get('track')
+
     # Get the current day of the week in full
     current_day_of_week = datetime.datetime.now().strftime('%A')
 
@@ -17,17 +18,17 @@ def get_info():
     utc_time = datetime.datetime.now(pytz.UTC).strftime('%Y-%m-%d %H:%M:%S')
 
     # GitHub repo URL and file URL
-    github_repo_url = 'https://github.com/your_username/your_repository'
-    github_file_url = os.path.join(github_repo_url, 'path/to/your/file.py')
+    github_repo_url = 'https://github.com/Khansmira/backend-api_projects-HNG/blob/master/main.py'
+    github_file_url = 'https://github.com/Khansmira/backend-api_projects-HNG.git'
 
     # Construct the response JSON
     response_data = {
         'Slack Name': slack_name,
         'Current Day of the Week': current_day_of_week,
         'Current UTC Time': utc_time,
-        'Track': 'backend',
+        'Track': track,
         'GitHub File URL': github_file_url,
-        'GitHub Repo URL': https://github.com/Khansmira/backend-api_projects-HNG.git,
+        'GitHub Repo URL': github_repo_url,
         'Status Code': 200
     }
 
