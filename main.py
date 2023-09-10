1fb352e249c6c7af7d94e1126779d920dcb7f30f
Khansmira <miriamakanwa@gmail.com>
2023-09-10 10:13:57 -0700

    fix: endpoint main function

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 from flask import Flask, request, jsonify
 import datetime
-import pytz
 
 app = Flask(__name__)
 
 # Define the route for the endpoint
-@app.route('/info', methods=['GET'])
+@app.route('/api', methods=['GET'])
 def get_info():
     # Get query parameters
     slack_name = request.args.get('slack_name')
     track = request.args.get('track')
 
     # Get the current day of the week in full
-    current_day_of_week = datetime.datetime.now().strftime('%A')
+    current_day_of_week = datetime.datetime.utcnow().strftime('%A')
 
     # Get the current UTC time
-    utc_time = datetime.datetime.now(pytz.UTC).strftime('%Y-%m-%d %H:%M:%S')
+    utc_time = datetime.datetime.now.now().strftime('%Y-%m-%dT %H:%M:%SZ')
 
     # GitHub repo URL and file URL
-    github_repo_url = 'https://github.com/Khansmira/backend-api_projects-HNG/blob/master/main.py'
-    github_file_url = 'https://github.com/Khansmira/backend-api_projects-HNG.git'
+    github_repo_url = 'https://github.com/Khansmira/backend-api_projects-HNG.git'
+    github_file_url = 'https://github.com/Khansmira/backend-api_projects-HNG/blob/master/main.py'
 
     # Construct the response JSON
     response_data = {
